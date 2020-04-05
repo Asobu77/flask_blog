54c840572888f7cbce524a684e1adfc2d54af230
Asobu77 <43177202+Asobu77@users.noreply.github.com>
2020-04-05 17:42:51 +0900

    import flask-login no implementation

--- flask_blog/views.py
+++ flask_blog/views.py
@@ -7,24 +7,32 @@ from flask_blog import app
 from flask import request, redirect, url_for, render_template, flash, session
 import sys
 
-
 @app.route('/')
 def show_entries():
+    # セッションがなければログインする
+    if not session.get('logged_in'):
+        return redirect('/login')
+
     return render_template('entries/index.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    error = None
     if request.method == 'POST':
         if request.form['username'] != app.config['USERNAME']:
             print('ユーザー名が異なります')
         elif request.form['username'] != app.config['PASSWORD']:
             print('パスワードが異なります')
         else:
+            # session更新
+            session['logged_in'] = True
             return redirect('/')
 
     return render_template('login.html')
 
 @app.route('/logout')
 def logout():
+    # session削除
+    session.pop('logged_in', None)
     return redirect('/')
 
