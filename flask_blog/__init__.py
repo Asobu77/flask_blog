4b6c4579f0e7af784b1942bf7c164c569ff58d64
Asobu77 <43177202+Asobu77@users.noreply.github.com>
2020-04-05 18:59:32 +0900

    db追加

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -1,10 +1,14 @@
 # flask_blog/__init__.py
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 # アプリケーション本体を作成。らしい。
 app = Flask(__name__)
-
 # config.pyの内容をconfigとして扱う
 app.config.from_object('flask_blog.config')
+
+# db
+db = SQLAlchemy(app)
+
 
 import flask_blog.views
