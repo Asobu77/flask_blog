d6d9704e0e9298bcf8f232d261269266b8741aad
Asobu77 <43177202+Asobu77@users.noreply.github.com>
2020-05-04 21:33:55 +0900

    体重変化グラフの追加

--- flask_blog/views/dashboard.py
+++ flask_blog/views/dashboard.py
@@ -2,7 +2,7 @@ from flask import request, redirect, url_for, render_template, flash, session, B
 from flask_blog import app, db
 from flask_blog.models.weights import Weight
 from flask_blog.views.views import login_required
-
+import datetime
 import sys
 
 dashboard = Blueprint('dashboard', __name__)
@@ -10,4 +10,13 @@ dashboard = Blueprint('dashboard', __name__)
 @dashboard.route('/', methods=['GET'])
 @login_required
 def top():
-    return render_template('dashboard/top.html')
+    # データをリストに格納していく
+    recode_date = list()
+    recode_weight = list()
+    Weights = Weight.query.order_by(Weight.date.asc()).all()
+    for i in Weights:
+        recode_date.append(datetime.datetime.strftime(i.date, "%m/%d"))
+        recode_weight.append(i.weight)
+    app.logger.debug(recode_date)
+    data = { 'date' : recode_date, 'weight' : recode_weight }
+    return render_template('dashboard/top.html', data = data)
